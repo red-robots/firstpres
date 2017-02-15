a57ef2aa52b0216c16eb018ca86ad8fa562c21a7
Fritz <fre.healy@gmail.com>
2017-02-15 17:01:34 -0500

    major templating footer and interior header

--- footer.php
+++ footer.php
@@ -14,13 +14,65 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="wrapper">
-			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'acstarter' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'acstarter' ), 'WordPress' ); ?></a>
-				<span class="sep"> | </span>
-				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'acstarter' ), 'acstarter', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-			</div><!-- .site-info -->
-	</div><!-- wrapper -->
+        <div class="row-1">
+            <div class="column-1">
+                <!--events-->
+            </div><!--.column-1-->
+            <div class="column-2">
+                <!--resources-->
+            </div><!--.column-2-->
+            <div class="column-3">
+                <!--give-->
+            </div><!--.column-3-->
+        </div><!--.row-1-->
+		<div class="row-2">
+			<div class="column-1">
+                <div class="column-1">
+                    <?php $google_maps = get_field("google_maps","option");
+                    if($google_maps):
+                        echo $google_maps;
+                    endif;?>
+                </div><!--.column-1-->
+                <div class="column-2">
+                    <?php $address = get_field("address","option");
+                    if($address):
+                        echo $address;
+                    endif;?>
+                </div><!--.column-2-->
+            </div><!-- .column-1 -->
+            <div class="column-2">
+                <div class="facebook">
+		            <?php $facebook=get_field("facebook_link","option");
+		            if($facebook):?>
+                        <img src="<?php echo get_template_directory_uri()."/images/social/facebook.png";?>" alt="facebook">
+		            <?php endif;?>
+                </div><!--.facebook-->
+                <div class="twitter">
+		            <?php $twitter=get_field("twitter_link","option");
+		            if($twitter):?>
+                        <img src="<?php echo get_template_directory_uri()."/images/social/twitter.png";?>" alt="twitter">
+		            <?php endif;?>
+                </div><!--.twitter-->
+                <div class="instagram">
+		            <?php $instagram=get_field("instagram_link","option");
+		            if($instagram):?>
+                        <img src="<?php echo get_template_directory_uri()."/images/social/instagram.png";?>" alt="instagram">
+		            <?php endif;?>
+                </div><!--.instagram-->
+                <div class="linkedin">
+		            <?php $linkedin=get_field("linkedin_link","option");
+		            if($linkedin):?>
+                        <img src="<?php echo get_template_directory_uri()."/images/social/linkedin.png";?>" alt="linkedin">
+		            <?php endif;?>
+                </div><!--.linkedin-->
+                <div class="vimeo">
+		            <?php $vimeo=get_field("vimeo_link","option");
+		            if($vimeo):?>
+                        <img src="<?php echo get_template_directory_uri()."/images/social/vimeo.png";?>" alt="vimeo">
+		            <?php endif;?>
+                </div><!--.vimeo-->
+            </div><!--.column-2-->
+	    </div><!-- .row-2 -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
---
+++ header-interior.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The header for theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ACStarter
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<header id="masthead" class="site-header" role="banner">
+		<div class="wrapper">
+			<div class="row-1">
+                <div class="new">
+                </div><!--.new-->
+                <div class="week">
+                </div><!--.week-->
+            </div><!--.row-1-->
+			<?php if(is_home()): ?>
+	            <h1 class="logo row-2">
+	            <a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri()."/images/logo.png";?>" alt="<?php bloginfo('name'); ?>"></a>
+	            </h1>
+	        <?php else: ?>
+	            <div class="logo row-2">
+	            <a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri()."/images/logo.png";?>" alt="<?php bloginfo('name'); ?>"></a>
+	            </div><!--.logo .row-2-->
+	        <?php endif; ?>
+
+			<nav id="site-navigation" class="main-navigation row-3" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'acstarter' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+	    </div><!-- wrapper -->
+	</header><!-- #masthead -->
+	<div id="content" class="site-content wrapper">
