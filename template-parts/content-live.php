d5e3f327eeaa162b158e220e443e82ea1f63159e
Fritz <fre.healy@gmail.com>
2017-02-23 16:18:41 -0500

    adding temporarily live streams

---
+++ template-parts/content-live.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Template part for displaying page content in single.php.
+ *
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-live"); ?>>
+    <?php $image = get_field("banner");
+    if($image):?>
+        <div class="row-1">
+            <img src="<?php echo $image['url'];?>" alt="<?php echo $image['alt'];?>">
+        </div><!--.row-2-->
+    <?php endif;?>
+    <div class="row-2 clear-bottom">
+        <div class="column-1">
+            <div class="title">
+                <header>
+                    <h1><?php the_title();?></h1>
+                </header>
+            </div><!--.title-->
+            <?php if(get_the_content()):?>
+                <div class="copy">
+                    <?php the_content();?>
+                </div><!--.copy-->
+            <?php endif;?>
+            <?php $live = get_field("live");
+            $past = get_field("past");
+            $past_title = get_field("past_title");
+            $live_title = get_field("live_title");
+            if($live):?>
+                <div class="live">
+		            <?php if($live_title):?>
+                        <h2><?php echo $live_title;?></h2>
+		            <?php endif;?>
+                    <div class="iframe-wrapper">
+                        <iframe allowfullscreen scrolling="no" frameborder="0" src="<?php echo $live;?>" width="100%" height="100%"></iframe>
+                    </div>
+                </div><!--.live-->
+            <?php endif;
+            if($past):?>
+                <div class="past">
+                    <?php if($past_title):?>
+                        <h2><?php echo $past_title;?></h2>
+                    <?php endif;?>
+                    <div class="iframe-wrapper">
+                        <iframe allowfullscreen scrolling="no" frameborder="0" src="<?php echo $past;?>" width="100%" height="100%"></iframe>
+                    </div>
+                </div><!--.past-->
+            <?php endif;?>
+        </div><!--.column-1-->
+        <div class="column-2">
+	        <?php get_sidebar('archive');?>
+        </div><!--.column-2-->
+    </div><!--.row-3-->
+</article><!-- #post-## -->
